49df2bd0312a611b96462dc1dcbb0c7e694b0d6c
elmove <elmove@protonmail.com>
2022-01-11 14:50:29 +0100

    Update scancomputers.sh

--- scancomputers.sh
+++ scancomputers.sh
@@ -3,7 +3,7 @@
 if [ $1 ]; then
 ip=$1
 for i in $(seq 1 255); do
-timeout 1 bash -c "ping -c 1 $1.$i > /dev/null 2>&1" && echo "$ip.$i - Active"
+timeout 1 bash -c "ping -c 1 $1.$i > /dev/null 2>&1" && echo "$ip.$i - Active" &
 done; wait
 else
 echo -e "\nUse ./scancomputers.sh <3 first octects of ip>\n"
